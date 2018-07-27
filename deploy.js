5d089acb6ddf0ce85219a28c5315e01693e50967
Heal <Healscm@gmail.com>
2018-07-27 16:51:26 +0800

    sd

--- deploy.js
+++ deploy.js
@@ -1,5 +1,3 @@
-v
-ddr http = require('http')
 var createHandler = require('github-webhook-handler')
 var handler = createHandler({ path: '/incoming', secret: 'myHashSecret' }) 
 // 上面的 secret 保持和 GitHub 后台设置的一致
